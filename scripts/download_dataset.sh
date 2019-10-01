5fe3fe18f8bbd774646a82e7aa3b7f072e720939
jquintas <jquintas@gmail.com>
2019-10-01 18:34:30 +0100

    Update links

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O `rospack find turtlebot3_datasets`/data/slam_easy.bag http://dantecloud.isr.tecnico.ulisboa.pt/index.php/s/ooAKYGEoZAR47NR/download
-wget -O `rospack find turtlebot3_datasets`/data/fixed_slam_easy.bag http://dantecloud.isr.tecnico.ulisboa.pt/index.php/s/aRt6CzFdEtSnHgE/download
+gdown -O `rospack find turtlebot3_datasets`/data/slam_easy.bag https://drive.google.com/uc?id=1a6csy_UKncrv7Eha5hWuWMAUSnbeJkWq
+gdown -O `rospack find turtlebot3_datasets`/data/fixed_slam_easy.bag https://drive.google.com/uc?id=186PfQ7CkX9EaTo1Sfuc9SekZ3-MX28M5
