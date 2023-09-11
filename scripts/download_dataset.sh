cf296ac3ad71c90bb7584e7de6572dbf5c9ba8bb
jspinto <joao.s.pinto@tecnico.ulisboa.pt>
2023-09-11 22:53:25 +0100

    updated links

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gdown -O `rospack find turtlebot3_datasets`/data/slam_easy.bag https://drive.google.com/uc?id=1a6csy_UKncrv7Eha5hWuWMAUSnbeJkWq
-gdown -O `rospack find turtlebot3_datasets`/data/fixed_slam_easy.bag https://drive.google.com/uc?id=186PfQ7CkX9EaTo1Sfuc9SekZ3-MX28M5
+path_to_pkg=`rospack find turtlebot3_datasets`/data
+gdown -O $path_to_pkg/ir_labs.tar.gz https://drive.google.com/uc?id=1-QdzXIoh0ltdDNGuffLqzsPZLnLwSjAj && tar -xvf $path_to_pkg/ir_labs.tar.gz -C $path_to_pkg
